01289399206e01ca03c1789cd96892575bd707b4
KleverKobold <104393766+KleverKobold@users.noreply.github.com>
2023-03-28 18:42:46 -0300

    finished portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Tilt from 'react-tilt'
+import {motion} from "framer-motion"
+
+import { styles } from '../styles'
+import { services } from '../constants'
+import {fadeIn, textVariant} from "../utils/motion"
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({index, title, icon}) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full" >
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75 )}
+                  className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card" >
+          <div
+              options={{
+                max: 45,
+                scale: 1,
+                speed: 450
+              }}
+              className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col ">
+                <img src={icon} alt={title}
+                      className="w-16 h-16 object-contain "/>
+                <h3 className='text-white text-[20px] font-bold text-center '>
+                  {title}
+                </h3>
+          </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText} >Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview </h2>
+      </motion.div>
+
+                             {/* direction, type, delay and duration */}
+      <motion.p variants={fadeIn("", "", 0.1, 1)}
+                className="mt-4 text-secondary text-[17px]
+                           max-w-3xl leading-[30px]"> 
+          I'm a Jr Software Developer with experience in Javascript, React.js,
+          Node.js, Express.js and SQL.
+          Currently i'm learning 3D design and visuals
+          in Three.js and TypeScript.<br></br> 
+          I'm an inquisitive learner with a great conviction
+          to acquire more knowledge,<br></br> 
+          an empathetic good team player and quick to adapt with a 
+          resilient mentality.<br></br>
+          Let's work together to bring new ideas to life!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+          {services.map((service, index)=> (
+                <ServiceCard key={service.title} index={index} {...service} />
+          ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper (About, "about")
